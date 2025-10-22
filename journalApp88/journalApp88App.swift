58b71e97f9fe8951bfef7e78f22795c6e6724851
joumana <alsagheir.joumana@gmail.com>
2025-10-22 19:36:43 +0300

    Initial Commit

---
+++ journalApp88/journalApp88App.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp88App.swift
+//  journalApp88
+//
+//  Created by Joumana Alsagheir on 22/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct journalApp88App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
